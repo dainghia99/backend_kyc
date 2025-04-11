bf3390561e02a3e3fa1d48c15da12374a6f040d5
dainghia99 <midvip99@gmail.com>
2025-04-11 15:47:41 +0700

    Fix bug

--- routes/kyc.py
+++ routes/kyc.py
@@ -5,6 +5,14 @@ import numpy as np
 from models import db, KYCVerification, User
 from utils.auth import token_required
 from utils.liveness import detect_blinks
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 kyc_bp = Blueprint('kyc', __name__)
 
